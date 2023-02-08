369c181c7c7bf80aac4886d84940ee2de46d13df
hodoos <motoboxx@naver.com>
2023-02-08 18:53:49 +0900

    [이종호] - 최초파일 추가

---
+++ src/test/java/com/hodoos/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodoos.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
